695d19b397ab4873292c2e05614c96d351b20ead
Diggo Silva <rdiggosilva@gmail.com>
2025-04-20 10:54:17 -0300

    Initial configuration

---
+++ Digimon/App/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Digimon
+//
+//  Created by Diggo Silva on 20/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+}
