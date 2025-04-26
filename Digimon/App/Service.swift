5786e92ad98af0930f9b1bb2d1fdfe1c487fff31
Diggo Silva <rdiggosilva@gmail.com>
2025-04-26 12:31:55 -0300

    Created Service file

--- Digimon/App/Service.swift
+++ Digimon/App/Service.swift
@@ -6,3 +6,71 @@
 //
 
 import Foundation
+
+protocol ServiceProtocol {
+    func getDigimons(page: Int, completion: @escaping(Result<[Digimon], DSError>) -> Void)
+}
+
+final class Service: ServiceProtocol {
+    
+    func getDigimons(page: Int, completion: @escaping(Result<[Digimon], DSError>) -> Void) {
+        fetchData(endpoint: .pagedDigimons(page: page), decondingType: DigimonResponse.self) { result in
+            switch result {
+            case .success(let digimonsResponse):
+                let digimons = digimonsResponse.content.map { content in
+                    Digimon(id: content.id, name: content.name, href: content.href, image: content.image)
+                }
+                completion(.success(digimons))
+                
+            case .failure(_):
+                completion(.failure(.digimonsFailed))
+            }
+        }
+    }
+    
+    private func fetchData<T: Decodable>(endpoint: DigiEndpoint, decondingType: T.Type, completion: @escaping(Result<T, DSError>) -> Void) {
+        guard let url = createURL(for: endpoint) else {
+            completion(.failure(.invalidData))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                if error != nil {
+                    completion(.failure(.networkError))
+                    return
+                }
+                
+                guard response is HTTPURLResponse else {
+                    completion(.failure(.networkError))
+                    return
+                }
+                
+                guard let data = data else {
+                    completion(.failure(.invalidData))
+                    return
+                }
+                
+                do {
+                    let decodedResponse = try JSONDecoder().decode(decondingType, from: data)
+                    completion(.success(decodedResponse))
+                } catch {
+                    completion(.failure(.failedDecoding))
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    private func createURL(for endpoint: DigiEndpoint) -> URL? {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "digi-api.com"
+        urlComponents.path = endpoint.path
+        
+        if let queryItems = endpoint.queryItems { urlComponents.queryItems = queryItems }
+        
+        print("DEBUG: URL: \(String(describing: urlComponents.url))")
+        return urlComponents.url
+    }
+}
