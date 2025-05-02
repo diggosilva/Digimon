af27563f527ff741b14156f5d3df30b759ea204f
Diggo Silva <rdiggosilva@gmail.com>
2025-05-02 15:08:04 -0300

    Created Details custom object

---
+++ Digimon/App/Model/Details.swift
@@ -0,0 +1,40 @@
+//
+//  Details.swift
+//  Digimon
+//
+//  Created by Diggo Silva on 02/05/25.
+//
+
+import Foundation
+
+class Details: CustomStringConvertible {
+    let id: Int
+    let name: String
+    let imageURLs: [String]
+    let fieldNames: [String]
+    let descriptions: [String]
+    let priorEvolutions: [String]
+    let nextEvolutions: [String]
+    
+    init(from response: DetailsResponse) {
+        self.id = response.id
+        self.name = response.name
+        self.imageURLs = response.images.map { $0.href }
+        self.fieldNames = response.fields.map { $0.field }
+        self.descriptions = response.descriptions.map { $0.description }
+        self.priorEvolutions = response.priorEvolutions.map { $0.digimon }
+        self.nextEvolutions = response.nextEvolutions.map { $0.digimon }
+    }
+    
+    var description: String {
+        return """
+        Details:
+        Name: \(name)
+        Images: \(imageURLs.joined(separator: ", "))
+        Fields: \(fieldNames.joined(separator: ", "))
+        Descriptions: \(descriptions.joined(separator: " | "))
+        Prior Evolutions: \(priorEvolutions.joined(separator: ", "))
+        Next Evolutions: \(nextEvolutions.joined(separator: ", "))
+        """
+    }
+}
