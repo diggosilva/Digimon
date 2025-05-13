735929df6b890495b636e550cd3d4536b821cd45
Diggo Silva <rdiggosilva@gmail.com>
2025-05-13 09:37:29 -0300

    Created tests for FeedViewModel

--- DigimonTests/FeedViewModelTests.swift
+++ DigimonTests/FeedViewModelTests.swift
@@ -1,5 +1,5 @@
 //
-//  DigimonTests.swift
+//  FeedViewModelTests.swift
 //  DigimonTests
 //
 //  Created by Diggo Silva on 12/05/25.
@@ -8,34 +8,51 @@
 import XCTest
 @testable import Digimon
 
-class MockFeedViewModel: FeedViewModelProtocol {
-    func numberOfItemsInSection() -> Int {
-        <#code#>
+class MockFeed: ServiceProtocol {
+    
+    var isSuccess: Bool = true
+    
+    func getDigimons(page: Int, completion: @escaping (Result<[Digimon], DSError>) -> Void) {
+        if isSuccess {
+            completion(.success([
+                Digimon(id: 1, name: "Alfamon", href: "", image: ""),
+                Digimon(id: 2, name: "Betamon", href: "", image: ""),
+            ]))
+        } else {
+            completion(.failure(.digimonsFailed))
+        }
     }
     
-    func cellForItem(at indexPath: IndexPath) -> Digimon {
-        <#code#>
-    }
-    
-    func searchBar(textDidChange searchText: String) {
-        <#code#>
-    }
-    
-    func fetchDigimons() {
-        <#code#>
-    }
-    
-    func getDigimons() -> [Digimon] {
-        <#code#>
-    }
-    
-    func observeState(_ observer: @escaping (FeedViewControllerStates) -> Void) {
-        <#code#>
-    }
+    func getDetails(of digimon: Digimon, completion: @escaping (Result<Details, DSError>) -> Void) {}
 }
 
 final class DigimonTests: XCTestCase {
     
+    func testWhenSuccess() {
+        let mockService = MockFeed()
+        let sut = FeedViewModel(service: mockService)
+        
+        sut.fetchDigimons()
+        XCTAssertEqual(sut.numberOfItemsInSection(), 2)
+        
+        let firstName = sut.cellForItem(at: IndexPath(item: 0, section: 0)).name
+        let secondId = sut.cellForItem(at: IndexPath(item: 1, section: 0)).id
+        XCTAssertEqual(firstName, "Alfamon")
+        XCTAssertEqual(secondId, 2)
+        
+        sut.searchBar(textDidChange: "Bet")
+        XCTAssertEqual(sut.numberOfItemsInSection(), 1)
+        
+        sut.searchBar(textDidChange: "")
+        XCTAssertEqual(sut.getDigimons().count, 2)
+    }
     
-    
+    func testWhenFailure() {
+        let mockService = MockFeed()
+        mockService.isSuccess = false
+        let sut = FeedViewModel(service: mockService)
+        
+        sut.fetchDigimons()
+        XCTAssertEqual(sut.numberOfItemsInSection(), 0)
+    }
 }
