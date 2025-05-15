ad1f7312bf77f36effeef109f103fe8c97ebc11b
Diggo Silva <rdiggosilva@gmail.com>
2025-05-15 10:18:05 -0300

    Refactored

---
+++ DigimonTests/FavoritesViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesViewModelTests.swift
+//  DigimonTests
+//
+//  Created by Diggo Silva on 14/05/25.
+//
+
+import Testing
+
+struct FavoritesViewModelTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
