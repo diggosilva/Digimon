4ba58718a874f5a879d3af1041f89dfac5832f27
Diggo Silva <rdiggosilva@gmail.com>
2025-06-22 10:53:16 -0300

    Refactored FeedViewModelTests file

--- DigimonTests/FeedViewModelTests.swift
+++ DigimonTests/FeedViewModelTests.swift
@@ -6,33 +6,61 @@
 //
 
 import XCTest
+import Combine
 @testable import Digimon
 
 class MockFeed: ServiceProtocol {
     
     var isSuccess: Bool = true
+    var shouldReturnEmpty: Bool = false
     
-    func getDigimons(page: Int, completion: @escaping (Result<[Digimon], DSError>) -> Void) {
+    func getDigimons(page: Int) async throws -> [Digimon] {
         if isSuccess {
-            completion(.success([
+            if shouldReturnEmpty {
+                return []
+            }
+            return [
                 Digimon(id: 1, name: "Alfamon", href: "", image: ""),
                 Digimon(id: 2, name: "Betamon", href: "", image: ""),
-            ]))
+            ]
         } else {
-            completion(.failure(.digimonsFailed))
+            throw DSError.digimonsFailed
         }
     }
     
-    func getDetails(of digimon: Digimon, completion: @escaping (Result<Details, DSError>) -> Void) {}
+    func getDetails(of digimon: Digimon) async throws -> Details { throw DSError.digimonsFailed }
 }
 
 final class DigimonTests: XCTestCase {
     
-    func testWhenSuccess() {
+    private var cancellables = Set<AnyCancellable>()
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        cancellables.removeAll()
+        super.tearDown()
+    }
+    
+    func testWhenSuccess() async throws {
         let mockService = MockFeed()
         let sut = FeedViewModel(service: mockService)
+        let expectation = XCTestExpectation(description: "State deveria ser .loaded")
+        
+        sut.statePublisher
+            .receive(on: RunLoop.main)
+            .sink { state in
+                if state == .loaded {
+                    expectation.fulfill()
+                }
+            }.store(in: &cancellables)
         
         sut.fetchDigimons()
+        
+        await fulfillment(of: [expectation], timeout: 2.0)
+        
         XCTAssertEqual(sut.numberOfItemsInSection(), 2)
         
         let firstName = sut.cellForItem(at: IndexPath(item: 0, section: 0)).name
@@ -47,12 +75,45 @@ final class DigimonTests: XCTestCase {
         XCTAssertEqual(sut.getDigimons().count, 2)
     }
     
-    func testWhenFailure() {
+    func testWhenFailure() async throws {
         let mockService = MockFeed()
         mockService.isSuccess = false
         let sut = FeedViewModel(service: mockService)
+        let expectation = XCTestExpectation(description: "State deveria ser .failed")
+        
+        sut.statePublisher
+            .receive(on: RunLoop.main)
+            .sink { state in
+                if state == .error {
+                    expectation.fulfill()
+                }
+            }.store(in: &cancellables)
+        
+        sut.fetchDigimons()
+        
+        await fulfillment(of: [expectation], timeout: 2.0)
+        
+        XCTAssertEqual(sut.numberOfItemsInSection(), 0)
+    }
+    
+    func testWhenEmptyDigimonsList() async throws {
+        let mockService = MockFeed()
+        mockService.shouldReturnEmpty = true
+        
+        let sut = FeedViewModel(service: mockService)
+        let expectation = XCTestExpectation(description: "State deveria ser .loading e depois sair sem alterar lista")
+        
+        sut.statePublisher
+            .receive(on: RunLoop.main)
+            .sink { state in
+                if state == .loading {
+                    expectation.fulfill()
+                }
+            }.store(in: &cancellables)
         
         sut.fetchDigimons()
         XCTAssertEqual(sut.numberOfItemsInSection(), 0)
+        
+        await fulfillment(of: [expectation], timeout: 2.0)
     }
 }
